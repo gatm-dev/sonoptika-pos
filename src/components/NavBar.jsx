5a4326d06a91239597c9c4eb42c8ca558d5db93e
gatm-dev <socialmedia.gatm@gmail.com>
2023-07-25 17:58:45 -0600

    Sq quita problema de eslint

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+
+// eslint-disable-next-line react/prop-types
+const NavBar = ({ children }) => {
+  return (
+    <>
+      <p>Hola Mundo</p>
+      {children}
+    </>
+  );
+};
+
+export default NavBar;
