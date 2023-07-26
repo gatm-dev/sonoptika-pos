22ba44d5ac144e8fb8eca020e34fd0494c7f6a2c
gatm-dev <socialmedia.gatm@gmail.com>
2023-07-26 01:33:07 -0600

    Cambios hasta tarea 1 de coderhouse

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,143 @@
-import NavBar from "./components/NavBar";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
+/* eslint-disable react/no-unknown-property */
 
-function App() {
+import { useState, useEffect, useRef } from "react";
+import {
+  Box,
+  Typography,
+  Paper,
+  Grid,
+  Container,
+  Fade,
+  Avatar,
+  IconButton,
+  Button,
+} from "@mui/material";
+import NavBar from "./components/NavBar";
+import { ShoppingBag as ShoppingBagIcon } from "@mui/icons-material";
+
+const fakeData = new Promise((resolve, reject) =>
+  setTimeout(
+    () => resolve(["Lentes oftálmicos", "Lentes de sol", "Micas graduadas"]),
+    300
+  )
+);
+
+const CustomCounter = ({ value = 0, setValue }) => {
+  const handleCount = (sign) =>
+    setValue(sign === "+" ? value + 1 : value - (value > 0 ? 1 : 0));
+
   return (
     <>
-      <NavBar>
-        <p>Bienvenidos</p>
-      </NavBar>
+      {value === 0 ? (
+        <Button
+          variant="outlined"
+          color="primary"
+          endIcon={<ShoppingBagIcon />}
+          onClick={() => handleCount("+")}
+        >
+          Agregar al carrito
+        </Button>
+      ) : (
+        <Fade in={value > 0} timeout={1000}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography variant="button" sx={{ mx: 2 }}>
+              En cesta
+            </Typography>
+            <IconButton onClick={() => handleCount("-")} size="small">
+              <Avatar variant="rounded">{"-"}</Avatar>
+            </IconButton>
+            <Typography variant="button" sx={{ mx: 2 }}>
+              {value}
+            </Typography>
+            <IconButton onClick={() => handleCount("+")} size="small">
+              <Avatar variant="rounded">{"+"}</Avatar>
+            </IconButton>
+          </Box>
+        </Fade>
+      )}
+    </>
+  );
+};
+
+function App() {
+  const splashRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [countExample, setCountExample] = useState(0);
+  const [menuItems, setMenuItems] = useState([]);
+
+  const handleMenuOpen = () => setMenuOpen(!menuOpen);
+
+  useEffect(() => {
+    fakeData.then((data) => setMenuItems(data));
+  }, []);
+
+  return (
+    <>
+      <Box
+        sx={{ backgroundColor: "#000", color: "#fff", textAlign: "center" }}
+        ref={splashRef}
+      >
+        <Typography variant="caption">
+          ¡Ofertas especiales todos los días!
+        </Typography>
+      </Box>
+      <Grid container>
+        {menuOpen && (
+          <Grid item xs={6} md={3}>
+            <Fade in={menuOpen} timeout={1000}>
+              <Paper
+                elevation={3}
+                sx={{
+                  height: `calc(100vh - ${splashRef.current.offsetHeight}px)`,
+                  backgroundColor: "#1f1f1f",
+                  color: "#fff",
+                }}
+                square={true}
+              >
+                <Container>
+                  <Typography variant="h5" sx={{ p: 2, color: "#c1c1c1", textShadow: "0px 5px 10px rgb(50, 50, 0)" }}>
+                    Menú
+                  </Typography>
+                  <hr />
+                  {menuItems.map((item, index) => (
+                    <Typography variant="body1" sx={{ p: 1 }} key={index}>
+                      {item}
+                    </Typography>
+                  ))}
+                </Container>
+              </Paper>
+            </Fade>
+          </Grid>
+        )}
+        <Grid item xs={menuOpen ? 6 : 12} md={menuOpen ? 9 : 12}>
+          <NavBar
+            menuOpen={menuOpen}
+            handleMenuOpen={handleMenuOpen}
+            shoppingCartCount={countExample}
+          />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h5" sx={{ textAlign: "center", mt: 2 }}>
+                ¡Bienvenido a la tienda!
+              </Typography>
+              <Box
+                sx={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  display: "flex",
+                }}
+              >
+                <CustomCounter
+                  value={countExample}
+                  setValue={setCountExample}
+                />
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,46 @@
+/* eslint-disable react/prop-types */
 
-// eslint-disable-next-line react/prop-types
-const NavBar = ({ children }) => {
+import { AppBar, Container, Toolbar, IconButton, Button } from "@mui/material";
+import {
+  Menu as MenuIcon,
+  MenuOpen as MenuOpenIcon,
+  ShoppingCartRounded as ShoppingCartIcon,
+} from "@mui/icons-material";
+import SonoptikaIcon from "../assets/3.png";
+import SonoptikaGradient from "../assets/DEGRADADO1.png";
+
+const NavBar = ({ menuOpen, handleMenuOpen, shoppingCartCount }) => {
   return (
-    <>
-      <p>Hola Mundo</p>
-      {children}
-    </>
+    <AppBar position="static" elevation={0} color="inherit" sx={{background: `url(${SonoptikaGradient})`}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <IconButton
+            sx={{ display: "flex", mr: 1, color: "#fff" }}
+            size="large"
+            color="inherit"
+            onClick={handleMenuOpen}
+          >
+            {menuOpen ? <MenuOpenIcon /> : <MenuIcon />}
+          </IconButton>
+          <IconButton
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+            size="large"
+            color="inherit"
+          >
+            <img src={SonoptikaIcon} style={{maxHeight: "100%", width: "4rem"}} />
+          </IconButton>
+          <Button
+            color="primary"
+            variant="filled"
+            startIcon={<ShoppingCartIcon />}
+            sx={{ display: "flex", ml: "auto", color: "#1c1c1" }}
+            disabled={shoppingCartCount === 0}
+          >
+            <span style={{color: "#fff"}}>{shoppingCartCount > 0 && shoppingCartCount}</span>
+          </Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
